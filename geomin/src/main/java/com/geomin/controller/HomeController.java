621596a124083ce843f5255fe20c12fd386ccb46
leeeunc <sosowl77@naver.com>
2023-08-23 16:00:54 +0900

    test 4tl

--- geomin/src/main/java/com/geomin/controller/HomeController.java
+++ geomin/src/main/java/com/geomin/controller/HomeController.java
@@ -26,6 +26,7 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		
 		// ㅎㅇ
+		//test
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
